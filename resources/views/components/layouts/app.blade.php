110b09a2a89937ce0f2ca35feb7ca9c1a6a32169
ALEXNET2019 <alex.belo.neto.2015@gmail.com>
2024-01-26 00:29:41 -0300

    Re-instalação Livewire 3

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,16 +1,17 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <meta charset="UTF-8">
+    <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
     <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
-    <link href="{{asset('css/bootstrap-icons-1.11.3/font/bootstrap-icons.min.css')}}" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('css/bootstrap-icons-1.11.3/font/bootstrap-icons.min.css')}}">
+
+    <title>{{ $title ?? 'Page Title' }}</title>
 </head>
 
 <body>
-
+    {{ $slot }}
     <script src="{{asset('js/bootstrap.bundle.min.js')}}" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 
---
+++ resources/views/livewire/counter.blade.php
@@ -0,0 +1,7 @@
+<div>
+    <h1>{{ $count }}</h1>
+
+    <button class="btn btn-success btn-sm" wire:click="increment">+</button>
+
+    <button class="btn btn-danger btn-sm" wire:click="decrement">-</button>
+</div>
